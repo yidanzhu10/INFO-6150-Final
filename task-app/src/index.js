5c30bd898cfbaa39813e7f4f4a7903b10dc26c90
yidanzhu10 <yzhu.uh@gmail.com>
2022-05-08 16:31:33 -0400

    To do list completed

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NotePage from './Components/NotePage';
+// import NotePage from './Components/NotePage';
+import TodoPage from './Components/TodoPage';
 
 // import Signup from './Components/Signup';
 // import Login from './Components/Login';
@@ -10,8 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Signup /> */}
-    {/* <Login /> */}
-    <NotePage />
+    {/* <Login />  */}
+    {/* <NotePage /> */}
+    <TodoPage />
   </React.StrictMode>
 );
 
