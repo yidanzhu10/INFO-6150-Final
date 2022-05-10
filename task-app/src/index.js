6ed21c1cb63a07ec85631914f0b798fff7c76f5a
Destination4298 <zhang.fengy@northeastern.edu>
2022-05-10 02:22:00 -0400

    match index

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import NotePage from './Components/NotePage';
+import NotePage from './Components/NotePage';
+import {Route, NavLink, HashRouter, Routes, Navigate} from 'react-router-dom';
 // import TodoPage from './Components/TodoPage';
 import Dashboard from './Dashboard/Dashboard';
 import Signup from './Components/Signup';
-// import Login from './Components/Login';
+import Login from './Components/Login';
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Signup />
-    {/* <Login />  */}
+  // <React.StrictMode>
+  //   <Signup />
+  //   {/* <Login />  */}
+  //   {/* <NotePage /> */}
+  //   {/* <TodoPage /> */}
+  //   <Dashboard />
+  // </React.StrictMode>
+
+  <HashRouter>
+    <Routes>
+      <Route path='/login' element = {<Login/>}/>
+      <Route path='/' element = {<Navigate replace to='/login'/>}/>
+      <Route path='/notepage' element={<NotePage/>}/>
+    </Routes>
+
+    {/* <Signup /> */}
+    {/* <Login /> */}
     {/* <NotePage /> */}
-    {/* <TodoPage /> */}
-    <Dashboard />
-  </React.StrictMode>
+   </HashRouter>
+
+  
 );
 
